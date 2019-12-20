644ce86fac32620854434f3ff407c1b3507cd504
Olivia-Deng <59091277+Olivia-Deng@users.noreply.github.com>
2019-12-21 01:48:11 +1100

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,3 @@
+1. import cluster_config.json from week8 and spark_tfh5.sh which submitted with the code when creat the cluster
+2. run the .ipynb file following the stage
+3. the result shows follow the Assignment 2 tasks.
---
+++ spark_tfh5.sh
@@ -0,0 +1,7 @@
+sudo yum -y install git
+sudo pip-3.6 install --quiet tensorflow-hub 
+sudo pip-3.6 install --quiet numpy
+sudo python3 -m nltk.downloader -d /usr/share/nltk_data all
+sudo pip-3.6 install --quiet pandas
+sudo pip-3.6 install --quiet scipy
+sudo pip-3.6 install --quiet sklearn
